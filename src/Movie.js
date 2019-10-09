6186f009b9f00835f7c797e827808568bc3ad148
cdbullones <cdbullones@gmail.com>
2019-10-09 11:22:38 -0400

    update key in component father Movies

--- src/Movie.js
+++ src/Movie.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Movie = (props) => (
     <div className="card" style={{'width': '100%', 'marginTop': '10px'}}>
-        <div className="card-body" key={props.movie.id}>
+        <div className="card-body">
             <h5 className="card-title">{props.movie.title}</h5>
             <h6 className="card-subtitle mb-2 text-muted">by {props.movie.year}</h6>
         </div>
--- src/Movies.js
+++ src/Movies.js
@@ -19,8 +19,8 @@ const Movies = () => (
     {({ loading, error, data }) => {
       if (loading) return <p>Loading...</p>;
       if (error) return <p>Error :(</p>;
-      return data.movies.map((currentMovie) => (
-          <Movie movie={currentMovie} />
+      return data.movies.map((currentMovie, key) => (
+          <Movie movie={currentMovie} key={key} />
         ));
     }}
   </Query>
